918b333ed51151bb5f6b2aa34dbb714f08ebabfc
Michał Ogiba <mifiszon@gmail.com>
2025-03-24 20:41:11 +0100

    serializer

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from api.models import User, Profile
+from api.serializer import UserSerializer, MyTokenSerializer
+from rest_framework.decorators import api_view
+from rest_framework import generics
+from rest_framework_simplejwt.views import TokenObtainPairView
 
-# Create your views here.
+class MyTokenView(TokenObtainPairView):
+    serializer_class = MyTokenSerializer
+    
+class RegisterView(generics.CreateAPIView):
+    serializer_class = 
